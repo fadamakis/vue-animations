e7944dd497473db36e9d7e80f5519108bd4a46d6
Fotis Adamakis <fotis316@yahoo.com>
2019-10-21 20:33:28 +0300

    Removed obsolete initial components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Vue.js Transitions and Animations Examples</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
